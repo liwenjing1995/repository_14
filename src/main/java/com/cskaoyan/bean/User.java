c4a02d666c59de310a45d16eb555423b2cbafa04
liwenjing1995 <443874266@qq.com>
2019-06-25 21:31:32 +0800

    14th first commit by liwenjing

---
+++ src/main/java/com/cskaoyan/bean/User.java
@@ -0,0 +1,22 @@
+package com.cskaoyan.bean;
+
+public class User {
+    int id;
+    String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
